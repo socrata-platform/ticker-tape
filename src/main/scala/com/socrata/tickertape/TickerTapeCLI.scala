d76f83933264cbb7c06af079223da938ce4cfe5f
Andrew 'Peaches' Gall <andrew.gall@socrata.com>
2016-12-15 10:39:05 -0800

    Oops variable shadowing

--- src/main/scala/com/socrata/tickertape/TickerTapeCLI.scala
+++ src/main/scala/com/socrata/tickertape/TickerTapeCLI.scala
@@ -37,9 +37,9 @@ object TickerTapeCLI extends StrictLogging {
     logger info s"Writing ${config.batchSize} metrics"
     val startTime = System.currentTimeMillis()
     0 until config.uniqueEntityIds foreach { i =>
-      0 until config.batchSize foreach { i =>
+      0 until config.batchSize foreach { j =>
         val entityId = new MetricIdParts(config.metricsEntityId,new MetricIdPart(s"$i"))
-        val metricName = s"fake-metric-$i"
+        val metricName = s"fake-metric-$j"
         val metricValue = 1
         logger debug s"Emitting metric with Entity ID: $entityId, Name: $metricName and Value: $metricValue"
         queue.create(entityId, Fluff(metricName), metricValue)
